e908bb4caf8741bf28ee8c9c446b1efa6d403784
rodrigo valentim <rodrigovalentim2008@gmail.com>
2025-02-26 10:59:16 -0300

    login

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,5 +1,5 @@
 import { logins } from './bd.js'; 
-let paginTrue = "http://127.0.0.1:5500/public/page/Visual.html";
+let paginTrue = "https://page-login-bice.vercel.app/public/page/Visual.html";
 
 let logar = document.querySelector(".logar");
 
